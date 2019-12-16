c9e3d53bac8db84dd8c6c7fb5965d8ad257690f1
Mike <lanselot777hailander@gmail.com>
2019-12-15 21:42:42 -0400

    First application web and Servlet

---
+++ HolaMundoServlets/src/main/java/Web/HolaMundo.java
@@ -0,0 +1,17 @@
+package Web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/HolaMundo")
+public class HolaMundo extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html/charset=UTF-8");        
+        PrintWriter out = response.getWriter();
+        out.println("Hola Mundo desde Servlets");
+    }
+}
